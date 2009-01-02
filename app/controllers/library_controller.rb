99f5f46851f08d17b6ee6e0ff50a840f9674699b
Ryan Niebur <ryanryan52@gmail.com>
2009-01-02 11:40:25 -0800

    comments

--- app/controllers/library_controller.rb
+++ app/controllers/library_controller.rb
@@ -1,20 +1,29 @@
 class LibraryController < ApplicationController
+  # check in books
   def checkin
     render :text => "not yet implemented"
   end
 
+  # check out books
   def checkout
     render :text => "not yet implemented"
   end
 
+  # search field to show_book and show_copy
   def search
     render :text => "not yet implemented"
   end
 
+  # enter new books and new copies ... three options
+  # * use existing book
+  # * pull from library of congress
+  # * manually enter
+  # it should have you enter the isbn, and then go down the list, finding the first possiblility, and making you use that. should also consider alternates.
   def cataloging
     render :text => "not yet implemented"
   end
 
+  # list of copies that are overdue
   def overdue
     render :text => "not yet implemented"
   end
@@ -24,7 +33,7 @@ class LibraryController < ApplicationController
     render :text => "not yet implemented"
   end
 
-  # link to labels, shows history
+  # link to labels, shows history, has a checkout button
   def show_copy
     render :text => "not yet implemented"
   end
