65b0c9783114583865df5fb30c0e45560ba5b573
Ryan Niebur <ryanryan52@gmail.com>
2009-01-02 11:22:01 -0800

    not yet implemented

--- app/controllers/library_controller.rb
+++ app/controllers/library_controller.rb
@@ -1,2 +1,21 @@
 class LibraryController < ApplicationController
+  def checkin
+    render :text => "not yet implemented"
+  end
+
+  def checkout
+    render :text => "not yet implemented"
+  end
+
+  def search
+    render :text => "not yet implemented"
+  end
+
+  def cataloging
+    render :text => "not yet implemented"
+  end
+
+  def overdue
+    render :text => "not yet implemented"
+  end
 end
