281ef2a535e69508d399ad2f2993adfaf5fa2191
Ryan Niebur <ryanryan52@gmail.com>
2009-01-24 12:20:14 -0800

    isbn is string

--- db/migrate/20081230211303_add_some_searchable_fields.rb
+++ db/migrate/20081230211303_add_some_searchable_fields.rb
@@ -3,7 +3,7 @@ class AddSomeSearchableFields < ActiveRecord::Migration
     add_column :books, :author, :string
     add_column :books, :title, :string
     add_column :books, :description, :text
-    add_column :books, :isbn, :integer
+    add_column :books, :isbn, :string # BAD. we need to just not use these.
   end
 
   def self.down
