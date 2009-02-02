9b24540f0201c11d8be6f7ad8e7592a6efee8be5
Ryan Niebur <ryanryan52@gmail.com>
2009-02-01 17:12:06 -0800

    move stuff

--- app/helpers/library_model_helper.rb
+++ app/helpers/library_model_helper.rb
@@ -1,17 +1,22 @@
 module LibraryModelHelper
   def self.included(base)
-    {
-      :author => [100, 'a'],
-      :title => [245, 'a'],
-      :description => [520, 'a'],
-      :isbn => [20, 'a']
-    }.each{|k,v|
+    marc_aliases.each{|k,v|
       define_method k do
         self.[](*v)
       end
       define_method (k.to_s + "=").to_sym do |val|
         self.[]=(val, *v)
       end
+    }
+  end
+
+  # may end up being used in conditions and such
+  def marc_aliases
+    {
+      :author => [100, 'a'],
+      :title => [245, 'a'],
+      :description => [520, 'a'],
+      :isbn => [20, 'a']
     }
   end
 end
