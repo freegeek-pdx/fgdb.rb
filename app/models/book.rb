ec07e0708e01aff37c745a00f7ac688e1f95401d
Ryan Niebur <ryanryan52@gmail.com>
2009-02-01 17:10:58 -0800

    note about the sucking down stuff sucking :P

--- app/models/book.rb
+++ app/models/book.rb
@@ -6,6 +6,13 @@ class Book < ActiveRecord::Base
   before_validation :suck_stuff_down
   before_create :add_copy
 
+  # TODO: this breaks stuff. the isbn that gets sucked down is a sucky
+  # prepresentation of array (because there's more than one), which
+  # don't work right with find_by_isbn. I should just rewrite
+  # find_by_isbn to use fields. how will this affect conditions?  I
+  # think I'll just remove the whole sucking down stuff, tho that
+  # scares me wrt search. I suppose that it will still work with SQL,
+  # just more complex. NOTE: it is stored correctly in the fields table.
   def suck_stuff_down
     for i in [:author, :isbn, :title, :description]
       eval("write_attribute(:#{i.to_s}, #{i.to_s})")
