15712be692d564868c2d660a49d4287899482df6
Ryan Niebur <ryanryan52@gmail.com>
2009-02-13 22:45:33 -0800

    simplify a bit

--- app/helpers/sidebar_helper.rb
+++ app/helpers/sidebar_helper.rb
@@ -23,53 +23,45 @@ module SidebarHelper
     # base
     aliases = {:a => :action, :c => :controller}
     sidebar_hash = OH.n
+    for i in ["hours", 'donations', 'sales', 'recyclings', 'disbursements', "reports", "contacts", "staff schedule", "library", "fgss"]
+      sidebar_hash[i] = OH.n
+    end
     # hours
     if has_role?("VOLUNTEER_MANAGER")
-      sidebar_hash.a("hours", OH.n("entry", :c => "volunteer_tasks"))
+      sidebar_hash["hours"]["entry"] = {:c => "volunteer_tasks"}
     elsif current_user and current_user.contact_id
-      sidebar_hash.a("hours", OH.n("entry", :c => "volunteer_tasks", :contact_id => current_user.contact_id))
+      sidebar_hash["hours"]["entry"] = {:c => "volunteer_tasks", :contact_id => current_user.contact_id}
     end
     # transactions
-    required_roles = {:donation => ["FRONT_DESK"], :sale => ["STORE"], :recycling => ["FRONT_DESK", "RECYCLINGS"], :disbursement => ['CONTACT_MANAGER', 'FRONT_DESK', 'STORE', 'VOLUNTEER_MANAGER']}
-    trans_list = ['donation', 'sale', 'recycling', 'disbursement']
-    trans_list.each do |i|
-      x = required_roles[i.to_sym]
+    {:donation => ["FRONT_DESK"], :sale => ["STORE"], :recycling => ["FRONT_DESK", "RECYCLINGS"], :disbursement => ['CONTACT_MANAGER', 'FRONT_DESK', 'STORE', 'VOLUNTEER_MANAGER']}.each do |i,x|
       if x.nil? || has_role?(*x)
-        pl = i.pluralize
-        trans = OH.n("entry", {:c => pl}, "search", {:c => pl, :a => 'search'})
-        sidebar_hash.a(pl, trans)
+        pl = i.to_s.pluralize
+        sidebar_hash[pl]["entry"] = {:c => pl}
+        sidebar_hash[pl]["search"] = {:c => pl, :a => 'search'}
       end
     end
     # reports
-    reports = OH.n
     ["income", "gizmos", "volunteering"].each do |x|
-      reports.a(x, :c => "reports", :a => x)
+      sidebar_hash["reports"][x] = {:c => "reports", :a => x}
     end
-    reports["over time"] = {:c => 'graphic_reports'}
-    sidebar_hash.a("reports", reports)
+    sidebar_hash["reports"]["over time"] = {:c => 'graphic_reports'}
     # contacts
-    contacts = OH.n
-    contacts.a("contacts", :c => "contacts") if has_role?('CONTACT_MANAGER', 'FRONT_DESK', 'STORE', 'VOLUNTEER_MANAGER')
-    contacts.a("dedup", :c => 'contact_duplicates') if has_role?('CONTACT_MANAGER')
-    contacts.a("duplicates list", :c => 'contact_duplicates', :a => "list_dups") if has_role?('CONTACT_MANAGER')
-    sidebar_hash.a("contacts", contacts)
+    sidebar_hash["contacts"]["contacts"] = {:c => "contacts"} if has_role?('CONTACT_MANAGER', 'FRONT_DESK', 'STORE', 'VOLUNTEER_MANAGER')
+    sidebar_hash["contacts"]["dedup"] = {:c => 'contact_duplicates'} if has_role?('CONTACT_MANAGER')
+    sidebar_hash["contacts"]["duplicates list"] = {:c => 'contact_duplicates', :a => "list_dups"} if has_role?('CONTACT_MANAGER')
     # staffsched
-    staffsched = OH.n("staffsched", "/staffsched")
-    staffsched.a("edit schedule", :c => "work_shifts") if should_show_edit_schedule and has_role?('SKEDJULNATOR')
-    sidebar_hash.a("staff schedule", staffsched) if should_show_schedule
+    sidebar_hash["staff schedule"]["staffsched"] = "/staffsched" if should_show_schedule
+    sidebar_hash["staff schedule"]["edit schedule"] = {:c => "work_shifts"} if should_show_edit_schedule and has_role?('SKEDJULNATOR')
     # library
-    lib = OH.n
     requires_librarian = ['overdue', 'labels']
     for i in ['lookup', 'overdue', 'cataloging', 'search', 'labels'] do
       if !requires_librarian.include?(i) or has_role?("LIBRARIAN")
-        lib.add(i, :c => "library", :a => i)
+        sidebar_hash["library"][i] = {:c => "library", :a => i}
       end
     end
-    sidebar_hash.add("library", lib) if should_show_library
     # fgss
-    fgss = OH.n("printme", :c => 'spec_sheets')
-    fgss.a("fix contract", :c => 'spec_sheets', :a => "fix_contract") if Contract.find(:all).length > 1 && has_role?("ADMIN") # TODO: fix this
-    sidebar_hash.add("fgss", fgss) if should_show_fgss
+    sidebar_hash["fgss"]["printme"] = {:c => 'spec_sheets'}
+    sidebar_hash["fgss"]["fix contract"] = {:c => 'spec_sheets', :a => "fix_contract"} if Contract.find(:all).length > 1 && has_role?("ADMIN") # TODO: fix this
     # done
     return aliases, sidebar_hash
   end
