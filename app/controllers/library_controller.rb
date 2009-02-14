d25c6c783ddaab567210537d4b85fd14b693b0b0
Ryan Niebur <ryanryan52@gmail.com>
2009-02-13 19:59:23 -0800

    lookup

--- app/controllers/library_controller.rb
+++ app/controllers/library_controller.rb
@@ -1,6 +1,6 @@
 class LibraryController < ApplicationController
   layout :with_sidebar
-  before_filter :authorized_only, :except => [:show_copy, :show_book, :search, :search_results]
+  before_filter :authorized_only, :except => [:show_copy, :show_book, :search, :search_results, :lookup]
   helper :library_labels
   include LibraryLabelsHelper
 
