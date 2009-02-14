40b73fee2df1ba7640bd71d6cc9d444ad6b56196
Ryan Niebur <ryanryan52@gmail.com>
2009-02-13 21:02:23 -0800

    make it all one thing

--- app/controllers/sidebar_links_controller.rb
+++ app/controllers/sidebar_links_controller.rb
@@ -2,14 +2,20 @@ class SidebarLinksController < ApplicationController
   layout :with_sidebar
 
   def fgss_moved
-    render :layout => false
+    thing_moved("FGss", "spec_sheets")
   end
 
   def staffsched_moved
-    render :layout => false
+    thing_moved("Skedjulnator", "staffsched")
   end
 
   def library_moved
-    render :layout => false
+    thing_moved("Library", "library")
+  end
+
+  def thing_moved(thing, controller)
+    @thing = thing
+    @url_ = "http://data/#{controller}"
+    render :action => "thing_moved", :layout => false
   end
 end
