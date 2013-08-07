8d2245c76dd06bd1cf0d6433e4116a2953daa814
Ryan Niebur <ryanryan52@gmail.com>
2013-08-06 23:08:47 -0700

    add indexes

---
+++ db/migrate/20130807055212_add_indexes_for_volskedjing.rb
@@ -0,0 +1,13 @@
+class AddIndexesForVolskedjing < ActiveRecord::Migration
+  def self.up
+    add_index :assignments, :volunteer_shift_id
+    add_index :volunteer_shifts, :volunteer_event_id
+    add_index :contact_volunteer_task_type_counts, :contact_id
+    add_index :contact_volunteer_task_type_counts, :volunteer_task_type_id, :name => "contact_volunteer_task_type_counts_vtask_id_fkey"
+    add_index :rosters_skeds, :sked_id
+    add_index :rosters_skeds, :roster_id
+  end
+
+  def self.down
+  end
+end
