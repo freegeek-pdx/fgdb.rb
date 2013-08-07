e966a1cfb92aa20021d117e723aae005746cb887
Ryan Niebur <ryanryan52@gmail.com>
2013-08-06 23:12:18 -0700

    one more index

--- db/migrate/20130807055212_add_indexes_for_volskedjing.rb
+++ db/migrate/20130807055212_add_indexes_for_volskedjing.rb
@@ -1,5 +1,6 @@
 class AddIndexesForVolskedjing < ActiveRecord::Migration
   def self.up
+    add_index :resources_volunteer_events, :volunteer_event_id
     add_index :assignments, :volunteer_shift_id
     add_index :volunteer_shifts, :volunteer_event_id
     add_index :contact_volunteer_task_type_counts, :contact_id
