383e7b8f3c6211141246c381f63671c125a3bc1e
Ryan Niebur <ryanryan52@gmail.com>
2013-08-10 17:41:43 -0700

    Fix contact_methods to validate and prevent when a <span is added (Closes: #36813)

--- app/models/contact_method.rb
+++ app/models/contact_method.rb
@@ -1,8 +1,18 @@
 class ContactMethod < ActiveRecord::Base
   belongs_to :contact_method_type
   belongs_to :contact
-  validates_associated :contact, :contact_method_type
-  validates_presence_of :contact, :contact_method_type
+  validates_associated :contact, :unless => :new_contact
+  validates_associated :contact_method_type
+  validates_presence_of :contact, :unless => :new_contact
+  validates_presence_of :contact_method_type
+
+  def new_contact
+    true # self.contact && self.contact.id.nil? # FIXME
+  end
+
+  def validate
+    errors.add('value', 'should not contain HTML, please disable Google Voice and other annoying browser plugins') if self.value && self.value.match(/<span/)
+  end
 
   def to_s
     desc = value
