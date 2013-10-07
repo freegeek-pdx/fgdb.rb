219d88495a73bfba98bb680fdc44a51baa06d1d6
Ryan Niebur <ryanryan52@gmail.com>
2013-10-06 17:35:00 -0700

    Fix crash while merging into old contacts with NULL values (Closes: #37840)

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,10 +8,10 @@ class User < ActiveRecord::Base
   validates_presence_of     :login, :email
   validates_presence_of     :password,                   :if => :password_required?
   validates_presence_of     :password_confirmation,      :if => :password_required?
-  validates_length_of       :password, :within => 4..40, :if => :password_required?
+  validates_length_of       :password, :within => 4..40, :if => :password_required?, :allow_nil => true
   validates_confirmation_of :password,                   :if => :password_required?
-  validates_length_of       :login,    :within => 3..40
-  validates_length_of       :email,    :within => 3..100
+  validates_length_of       :login,    :within => 3..40, :allow_nil => true
+  validates_length_of       :email,    :within => 3..100, :allow_nil => true
   validates_uniqueness_of   :login, :email, :case_sensitive => false
   validates_uniqueness_of   :cashier_code, :if => :cashier_code
   validates_format_of       :login, :with => /[^0-9]/, :message => "must contain a non-numeric character"
