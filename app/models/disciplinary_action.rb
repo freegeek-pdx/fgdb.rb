18696dc7835480cba072cd6a5350fd5d2d677632
Ryan Niebur <ryanryan52@gmail.com>
2013-12-06 23:01:57 -0800

    Add to_s for DA

--- app/models/disciplinary_action.rb
+++ app/models/disciplinary_action.rb
@@ -3,6 +3,11 @@ class DisciplinaryAction < ActiveRecord::Base
   def self.cashierable
     true
   end
+
+  def to_s
+    "Contact ##{contact_id}"
+  end
+
   has_and_belongs_to_many :disciplinary_action_areas
   belongs_to :contact
   named_scope :not_disabled, :conditions => ["disabled = 'f'"]
