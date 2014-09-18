0e03ebce0cd4acbc45991f39135aaa4ed4f15219
Ryan Niebur <ryanryan52@gmail.com>
2014-09-17 22:00:10 -0700

    Fix a script

--- script/pull_rt_metadata.sh
+++ script/pull_rt_metadata.sh
@@ -6,6 +6,7 @@ TEMPF=$(mktemp)
 
 sudo ssh -i /root/.ssh/wasp_key root@art /root/fgdb.rb/script/generate_rt_metadata.sh > $TEMPF
 if [ -n "$(cat $TEMPF)" ]; then
+    rm -f /var/www/fgdb.rb/config/rt_metadata.txt
     mv $TEMPF /var/www/fgdb.rb/config/rt_metadata.txt
     sudo fix-permissions
 fi
