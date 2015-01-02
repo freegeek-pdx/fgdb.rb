d575ca0f06406a0485572d406babd9cf2984e7c0
Ryan Niebur <ryanryan52@gmail.com>
2015-01-02 10:15:05 -0800

    Adjust suggested fees and rank

--- db/migrate/20150102175940_new_fee.rb
+++ db/migrate/20150102175940_new_fee.rb
@@ -1,8 +1,22 @@
 class NewFee < ActiveRecord::Migration
   def self.up
-    gt = GizmoType.find_by_name('monitor_crt')
-    gt.required_fee_cents = 1000
-    gt.save
+    for k, v in {'monitor_crt' => 1000}
+      gt = GizmoType.find_by_name(k)
+      gt.required_fee_cents = v
+      gt.save!
+    end
+
+    for k, v in {'tv_lcd' => 600, 'monitor_lcd' => 500, 'system_lcd' => 800, 'system' => 800, 'keyboard' => 300, 'mouse' => 300, 'misc_item' => 500, 'bag_box_misc' => 500, 'ups' => 600, 'smart_phone' => 300, 'server' => 600, 'net_device' => 200}
+      gt = GizmoType.find_by_name(k)
+      gt.suggested_fee_cents = v
+      gt.save!
+    end
+
+    for k, v in {'printer' => 14, 'keyboard' => 15, 'mouse' => 16}
+      gt = GizmoType.find_by_name(k)
+      gt.rank = v
+      gt.save!
+    end    
   end
 
   def self.down
